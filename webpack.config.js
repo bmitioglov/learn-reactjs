4394731cf4d1e2981aa5e3afb240167cd1687507
Boris Mitioglov <boris_mitioglov@epam.com>
2021-02-12 15:40:59 -0800

    hometask-2 added

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+const dotenv = require('dotenv').config( {
+    path: path.join(__dirname, '.env')
+} );
+
+module.exports = dotenv.parsed.ENV === 'development'
+    ? require ('./webpack.config.dev')
+    : require ('./webpack.config.prod');
