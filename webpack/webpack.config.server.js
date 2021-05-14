0b7ed84cd5dd6d0b846abab2118f437a1f1fb1b5
Boris Mitioglov <boris_mitioglov@epam.com>
2021-05-14 12:33:13 -0700

    homework-10: webpack fixes

--- webpack/webpack.config.server.js
+++ webpack/webpack.config.server.js
@@ -3,6 +3,7 @@ const nodeExternals = require('webpack-node-externals');
 const common = require('./webpack.config.common');
 
 module.exports = merge.merge(common, {
+  mode: 'development',
   name: 'server',
   target: 'node',
   entry: './src/server/server.js',
@@ -13,19 +14,23 @@ module.exports = merge.merge(common, {
   },
   module: {
     rules: [
-      {
-        test: /\.css$/,
-        include: /src/,
-        use: [
-          {
-            loader: 'css-loader/locals', // It doesn't embed CSS but only exports the identifier mappings.
-            options: {
-              modules: true,
-              localIdentName: '[name]-[hash:5]',
-            },
-          },
-        ],
-      },
+      // {
+      //   test: /\.css$/,
+      //   include: /src/,
+      //   use: [
+      //     {
+      //       loader: 'css-loader/locals', // It doesn't embed CSS but only exports the identifier mappings.
+      //       options: {
+      //         modules: true,
+      //         localIdentName: '[name]-[hash:5]',
+      //       },
+      //     },
+      //   ],
+      // },
+      {test: /\.(js)$/, use: 'babel-loader'},
+      {test: /\.css$/, use: ['style-loader', 'css-loader']},
+      {test: /\.(gif|svg|jpg|png)$/, use: 'file-loader'},
+      {test: /\.jsx?$/, resolve: { extensions: ['.js', '.jsx'] }, use: 'babel-loader'},
     ],
   },
 });
