a6e4d8f3989895b91a5bdf1bbfb846461ed3e557
Boris Mitioglov <boris_mitioglov@epam.com>
2021-05-14 12:36:08 -0700

    homework-10: webpack fixes 2

--- webpack/webpack.config.common.js
+++ webpack/webpack.config.common.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const reactLoadableTransformer = require('react-loadable-ts-transformer');
 
 const isDevMod = process.env.NODE_ENV === 'development';
@@ -19,58 +18,6 @@ module.exports = {
   
   module: {
     rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        loader: 'awesome-typescript-loader',
-        options: {
-          transpileOnly: true,
-          getCustomTransformers: () => ({
-            before: [reactLoadableTransformer],
-          }),
-        },
-      },
-      
-      {
-        // test: /\.s?css$/,
-        test: /\.(scss|css)$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              // publicPath: '/dist/',
-              sourceMap: true,
-            },
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-        ],
-      },
-      
-      {
-        test: /\.(png|svg|jpe?g|gif)$/,
-        include: /assets/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'assets/',
-              publicPath: 'assets/',
-            },
-          },
-        ],
-      },
     ],
   },
   
